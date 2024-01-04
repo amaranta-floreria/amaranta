b0a97b7927c1ec50b430bef5633af45ff445bc63
Roberto Avalos <rovalos@gmail.com>
2024-01-03 19:28:45 -0600

    fixed image sized and links

--- src/app/cuidados-de-ramos/page.tsx
+++ src/app/cuidados-de-ramos/page.tsx
@@ -1,20 +1,10 @@
-'use client'
-import { Pagination } from 'swiper/modules';
-import localFont from 'next/font/local'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import Image from 'next/image';
-
-const avenir = localFont({ src: '../../../public/fonts/avenir_next.ttf' })
+import Image from "next/image"
 
 export default function BouquetCare() {
 	return (
-		<div className={'h-screen flex flex-col items-center justify-center animate-pulse font-semibold ' + avenir.className}>
+		<div className='h-screen flex flex-col items-center justify-center font-bold'>
 			<Image
-				className="object-none object-center"
+				className="w-full max-h-[700px]"
 				src="/bouquet_care.jpeg"
 				alt="amaranta_logo Logo"
 				width={0}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const avenir = localFont({ src: '../../public/fonts/avenir_next.ttf' })
 export default function Home() {
 	return (
 		<div className={'h-screen flex flex-col items-center justify-center font-bold ' + avenir.className}>
-			<div className='flex flex-row space-x-4'>
+			<div className='flex flex-col sm:flex-row space-x-4 space-y-4 sm:space-y-0 '>
 				<Link className='text-base tracking-wide' href="/informacion-bancaria">Informacion bancaria</Link>
 				<Link className='text-base tracking-wide' href="/cuidados-de-ramos">Cuidado de ramos</Link>
 				<Link className='text-base tracking-wide' href="/cuidados-de-cajas">Cuidado de cajas</Link>
