2a12cf6562dd35b7502ee9e7fd789f97fb7ad58e
Roberto Avalos <robertoavalos@Mac-mini-de-Vanessa.local>
2025-09-17 20:47:21 -0600

    added ads scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			<head>
+				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5070436982429047" crossorigin="anonymous"></script>
+			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	)
