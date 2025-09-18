a4d0e6ded1f6cc27d54c3de214c16c67c689f1c0
Roberto Avalos <robertoavalos@Mac-mini-de-Vanessa.local>
2025-09-17 20:50:53 -0600

    fixed issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5070436982429047" crossorigin="anonymous"></script>
+				<script 
+				async 
+				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5070436982429047" 
+				crossOrigin="anonymous"/>
 			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
