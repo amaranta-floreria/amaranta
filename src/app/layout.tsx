ce447a6c82a99a3024690974e55494a709b3f5a5
Vanessa Urzúa <rovalos@gmail.com>
2026-04-26 18:41:37 -0600

    eliminado script de ads de google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,12 +16,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<head>
-				<script 
-				async 
-				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5070436982429047" 
-				crossOrigin="anonymous"/>
-			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
 	)
